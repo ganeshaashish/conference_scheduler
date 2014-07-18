3fbf98d6ca34a32ee5e796070d120d4012709e68
ganeshaashish <ashishchaudhary.j@gmail.com>
2014-07-18 17:51:38 +0530

    Update Tests.java
    Unit test class

--- src/test/Tests.java
+++ src/test/Tests.java
@@ -3,7 +3,8 @@ package test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -13,31 +14,88 @@ import utils.PresenterInfo;
 public class Tests 
 {
 	@Test
-	public void voidTestSchedule()
+	public void voidTestMaximizeThePresenters()
 	{
-		List<PresenterInfo> pi_list = new ArrayList<PresenterInfo>();
-		pi_list.add(new PresenterInfo("P1", 1, 200));
-		pi_list.add(new PresenterInfo("P2", 2, 120));
-		pi_list.add(new PresenterInfo("P3", 1, 100));
-		pi_list.add(new PresenterInfo("P4", 1, 200));
-		pi_list.add(new PresenterInfo("P5", 2, 100));
-		pi_list.add(new PresenterInfo("P6", 4, 100));
+		Map<String, PresenterInfo> pi_map = new HashMap<String, PresenterInfo>();
+		pi_map.put("P1", new PresenterInfo("P1", 1, 200));
+		pi_map.put("P2", new PresenterInfo("P2", 1, 120));
+		pi_map.put("P3", new PresenterInfo("P3", 1, 100));
+		pi_map.put("P4", new PresenterInfo("P4", 1, 200));
+		pi_map.put("P5", new PresenterInfo("P5", 2, 100));
+		pi_map.put("P6", new PresenterInfo("P6", 4, 100));
 		
 		int conference_hours = 8;
 		int number_of_slots = 3;
-		ArrayList<String> response = ConferenceScheduler.scheduler(conference_hours, number_of_slots, pi_list);
-		assertEquals("P3, P5, P6, $300", response.get(0));
+		ArrayList<String> response = ConferenceScheduler.maximize_presenter_scheduler(conference_hours, number_of_slots, pi_map);
+		assertEquals(true, response.contains("P4, P6, P1, P3, P2, $720"));
 	}
 	@Test
-	public void voidTestNotEnoughPresenters()
+	public void voidTestNotEnoughPresenters1()
 	{
-		List<PresenterInfo> pi_list = new ArrayList<PresenterInfo>();
-		pi_list.add(new PresenterInfo("P1", 1, 200));
-		pi_list.add(new PresenterInfo("P2", 2, 120));
+		Map<String, PresenterInfo> pi_map = new HashMap<String, PresenterInfo>();
+		pi_map.put("P1", new PresenterInfo("P1", 1, 200));
+		pi_map.put("P2", new PresenterInfo("P2", 2, 120));
 		
 		int conference_hours = 8;
 		int number_of_slots = 3;
-		ArrayList<String> response = ConferenceScheduler.scheduler(conference_hours, number_of_slots, pi_list);
+		ArrayList<String> response = ConferenceScheduler.maximize_presenter_scheduler(conference_hours, number_of_slots, pi_map);
 		assertEquals("Not enough presenters", response.get(0));
+	}
+	@Test
+	public void voidTestNotEnoughPresenters2()
+	{
+		//*** Hours < conference_hours case *************************************
+		Map<String, PresenterInfo> pi_map = new HashMap<String, PresenterInfo>();
+		pi_map.put("P1", new PresenterInfo("P1", 1, 200));
+		pi_map.put("P2", new PresenterInfo("P2", 2, 120));
+		pi_map.put("P2", new PresenterInfo("P2", 2, 120));
+		
+		int conference_hours = 8;
+		int number_of_slots = 3;
+		ArrayList<String> response = ConferenceScheduler.maximize_presenter_scheduler(conference_hours, number_of_slots, pi_map);
+		assertEquals("Not enough presenters", response.get(0));
+	}
+	@Test
+	public void voidTestMinimizeTheCost()
+	{
+		Map<String, PresenterInfo> pi_map = new HashMap<String, PresenterInfo>();
+		pi_map.put("P1", new PresenterInfo("P1", 1, 200));
+		pi_map.put("P2", new PresenterInfo("P2", 1, 120));
+		pi_map.put("P3", new PresenterInfo("P3", 1, 100));
+		pi_map.put("P4", new PresenterInfo("P4", 1, 200));
+		pi_map.put("P5", new PresenterInfo("P5", 2, 100));
+		pi_map.put("P6", new PresenterInfo("P6", 4, 100));
+		
+		int conference_hours = 8;
+		int number_of_slots = 3;
+		ArrayList<String> response = ConferenceScheduler.minimize_the_cost(conference_hours, number_of_slots, pi_map);
+		assertEquals(true, response.contains("P5, P6, P3, P2, $420"));
+	}
+	@Test
+	public void voidTestMinimizeTheCost_NotEnoughPresenter()
+	{
+		Map<String, PresenterInfo> pi_map = new HashMap<String, PresenterInfo>();
+		pi_map.put("P1", new PresenterInfo("P1", 1, 200));
+		pi_map.put("P2", new PresenterInfo("P2", 1, 120));
+		pi_map.put("P3", new PresenterInfo("P3", 1, 100));
+		pi_map.put("P4", new PresenterInfo("P4", 1, 200));
+		pi_map.put("P5", new PresenterInfo("P5", 2, 100));
+		
+		int conference_hours = 8;
+		int number_of_slots = 3;
+		ArrayList<String> response = ConferenceScheduler.minimize_the_cost(conference_hours, number_of_slots, pi_map);
+		assertEquals(true, response.contains("Not enough presenters"));
+	}
+	@Test
+	public void voidTestMinimizeTheCost_NotEnoughPresenter2()
+	{
+		Map<String, PresenterInfo> pi_map = new HashMap<String, PresenterInfo>();
+		pi_map.put("P1", new PresenterInfo("P1", 4, 200));
+		pi_map.put("P2", new PresenterInfo("P2", 4, 120));
+		
+		int conference_hours = 8;
+		int number_of_slots = 3;
+		ArrayList<String> response = ConferenceScheduler.minimize_the_cost(conference_hours, number_of_slots, pi_map);
+		assertEquals(true, response.contains("Not enough presenters"));
 	}
 }
